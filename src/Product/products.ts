46e879eef1476baf13f071704072badaf7267a10
JasonLyy <ly.jason.1999@gmail.com>
2021-08-22 23:08:45 +1000

    Add products

---
+++ src/Product/products.ts
@@ -0,0 +1,22 @@
+export const products = {
+  iPad: {
+    sku: "ipd",
+    name: "Super iPad",
+    price: 549.99,
+  },
+  macBook: {
+    sku: "mbp",
+    name: "MacBook Pro",
+    price: 1399.99,
+  },
+  appleTv: {
+    sku: "atv",
+    name: "Apple TV",
+    price: 109.5,
+  },
+  vgaAdapter: {
+    sku: "vga",
+    name: "VGA adapter",
+    price: 30.0,
+  },
+};
